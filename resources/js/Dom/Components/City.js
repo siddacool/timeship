4f9876abd6bc0058996d2b9eae041d56f5e979fe
Siddhesh Mangela <sidthecool007@gmail.com>
2018-05-24 11:24:41 +0530

    city names skeleton added

--- resources/js/Dom/Components/City.js
+++ resources/js/Dom/Components/City.js
@@ -5,13 +5,17 @@ export default function (param) {
 
   return `
     <li class="city" data-id="${param.city_id}">
-      <span class="city__name">${param.name}</span>
-      <span class="city__country">${param.country}</span>
-      <span class="city__country-name">${param.country_name}</span>
-      <span class="city__time" data-timezone="${param.timezone}">...</span>
-      <span class="city__time city__time--24" data-timezone="${param.timezone}">...</span>
-      <span class="city__timezone">${param.timezone}</span>
-      ${deleteBtn.Render()}
+      <div class="city__half">
+        <span class="city__name">${param.name}</span>
+        <span class="city__country-name">${param.country_name}</span>
+        <span class="city__country">(${param.country})</span>
+      </div>
+      <div class="city__half">
+        <span class="city__time" data-timezone="${param.timezone}">...</span>
+        <span class="city__time city__time--24" data-timezone="${param.timezone}">...</span>
+        <span class="city__timezone">${param.timezone}</span>
+        ${deleteBtn.Render()}
+      </div>
     </li>
   `;
 }
