12110062cdf6d7ff16126dfea0c0ad7e8bcc47ed
Siddhesh Mangela <sidthecool007@gmail.com>
2022-01-27 15:46:49 +0530

    Virtual fixed

--- src/components/PreviewList/index.tsx
+++ src/components/PreviewList/index.tsx
@@ -1,11 +1,16 @@
 import type { Component } from 'solid-js';
-import { previewList } from '../../store';
+import { onMount } from 'solid-js';
+import { previewList, poplulateAllItemsToPreviewList } from '../../store';
 import TimeCard from '../TimeCard';
 import styles from './style.module.css';
 import { VirtualContainer } from '@minht11/solid-virtual-container';
 
 const PreviewList: Component = () => {
   let scrollTargetElement!: HTMLDivElement;
+
+  onMount(() => {
+    poplulateAllItemsToPreviewList();
+  });
 
   return (
     <div style={{ overflow: 'auto' }} ref={scrollTargetElement} class={styles.PreviewList}>
