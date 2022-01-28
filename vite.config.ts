d5716b158bd18e0b76ded579ab9e19199e463f3e
Siddhesh Mangela <sidthecool007@gmail.com>
2022-01-28 15:42:10 +0530

    pwa added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), VitePWA({})],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
