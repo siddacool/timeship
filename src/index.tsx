9675e5a9bc3ede68c53c0fc63416c81c9c440d1c
Siddhesh Mangela <sidthecool007@gmail.com>
2022-01-28 15:55:18 +0530

    sw configured

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
+import { registerSW } from 'virtual:pwa-register';
 import { render } from 'solid-js/web';
 
 import './index.css';
 import App from './App';
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,39 @@ import solidPlugin from 'vite-plugin-solid';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin(), VitePWA({})],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'data.json'],
+      manifest: {
+        name: 'Timeship',
+        short_name: 'Timeship',
+        description:
+          'A world clock at your fingertips. Timeship displays time from cities all around the globe',
+        theme_color: '#054fdb',
+        display: 'standalone',
+        background_color: '#054fdb',
+        icons: [
+          {
+            src: 'img/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'img/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'img/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
