694c4d968eee7e690160e65f049ebe6a96a5b497
Siddhesh Mangela <sidthecool007@gmail.com>
2022-02-02 15:36:34 +0530

    layerd waves error fixed

--- src/components/TimeCard/BackgroundWave.tsx
+++ src/components/TimeCard/BackgroundWave.tsx
@@ -1,8 +1,11 @@
 import type { Component } from 'solid-js';
 import styles from './style.module.css';
+import layeredWaves from '../../assets/layered-waves.png';
 
 const BackgroundWave: Component = () => {
-  return <div class={styles.BackgroundWave}></div>;
+  return (
+    <div class={styles.BackgroundWave} style={`background-image: url(${layeredWaves});`}></div>
+  );
 };
 
 export default BackgroundWave;
